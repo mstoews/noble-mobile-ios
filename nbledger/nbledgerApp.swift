c264df76e68b7a8bf44d1f7f1d8f545c320b6019
mstoews <mstoews@hotmail.com>
2026-03-31 10:34:21 -0400

    Initial Commit

---
+++ nbledger/nbledgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  nbledgerApp.swift
+//  nbledger
+//
+//  Created by Murray Toews on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct nbledgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nbledgerTests/nbledgerTests.swift
@@ -0,0 +1,19 @@
+//
+//  nbledgerTests.swift
+//  nbledgerTests
+//
+//  Created by Murray Toews on 3/31/26.
+//
+
+import Testing
+@testable import nbledger
+
+struct nbledgerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
